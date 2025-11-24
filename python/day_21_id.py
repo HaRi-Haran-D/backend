a4240d3052dd5c17a903e3e89ca1a6e21b3f820a
HaRi-Haran-D <hvj58128@gmail.com>
2025-11-24 18:03:57 +0530

    Daily Update

---
+++ python/day_21_id.py
@@ -0,0 +1,7 @@
+a=[1,2,3,4,5]
+id(a)
+2055927783616
+b=[1,2,3,4,5]
+id(b)
+2055972474432
+#both the list has same values but different id because list values are considered as object so it has different id and also list values are mutable.
---
+++ python/day_21_polymorphism.py
@@ -0,0 +1,63 @@
+#polymorphism
+#operator overriding
+
+class Box:
+    def __init__(self,length):
+        self.length=length
+
+    def __add__(self,other):
+        return Box(self.length + other.length)
+
+b1=Box(10)
+b2=Box(20)
+b3=b1+b2
+print(b3.length)
+
+class Salary:
+    def __init__(self,salary):
+        self.salary=salary
+    def __mul__(self,other):
+        return Salary(self.salary * other.salary)
+a1=Salary(10)
+a2=Salary(28)
+a3=a1*a2
+print(a3.salary)
+
+#Method Overriding
+#it means it refers defining a method in a subclass with its same name as a method as a sub class this is said to run time poly
+#we can always overwrite your parent class method when you have special or differrnt   that is i as to change and exchange the parent class behaviours
+#to change parent class behaviour
+#to extend parent class behaviour
+#to achieve runtime polymorphism
+
+class Parent:
+    def show(self):
+        print('This is parent')
+    def display(self):
+        print('display parent')
+
+class Child(Parent):
+    def show(self):
+        print('This is child')
+    def display(self):
+        super().display()
+        print('display child')
+
+a=Child()
+a.show()
+a.display()
+
+#regular expression
+#we are going to 
+
+
+
+
+
+
+
+
+
+
+
+
