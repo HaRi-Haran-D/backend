edad2c9e521adc5b67cf39f765b7e4d71fe1a7c1
HaRi-Haran-D <hvj58128@gmail.com>
2026-05-09 18:48:32 +0530

    Python Practice

--- python/Mock/pdf.py
+++ python/Mock/pdf.py
@@ -1,11 +1,22 @@
-from pypdf import PdfReader
+# from pypdf import PdfReader
+#
+# reader = PdfReader(r'C:\Users\Admin\Downloads\example.pdf')
+# page = reader.pages[0]
+# all_text = ""
+#
+# for page in reader.pages:
+#     all_text += page.extract_text() + "\n"
+#
+# print("Combined text from all pages:")
+# print(all_text)
 
-reader = PdfReader(r'C:\Users\Admin\Downloads\example.pdf')
-page = reader.pages[0]
-all_text = ""
 
-for page in reader.pages:
-    all_text += page.extract_text() + "\n"
+n = 6
+print(n, end="")
 
-print("Combined text from all pages:")
-print(all_text)
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n = (3 * n) + 1
+    print(f", {n}", end="")
